201db29dc31fd4f369882a5c1b0526597cae9c77
Bernhard Vallant <bernhard@vallant.org>
2010-07-02 12:38:10 +0200

    + added getting by group support

--- flickr/cms_plugins.py
+++ flickr/cms_plugins.py
@@ -11,7 +11,7 @@ from models import *
 
 FLICKR_IMAGE_URL = "http://farm%(farm)s.static.flickr.com/%(server)s/%(id)s_%(secret)s_%(size)s.jpg"
 FLICKR_URL = "http://www.flickr.com/photos/%(user_id)s/%(id)s"
-
+FLICKR_GROUP_URL = "http://www.flickr.com/groups/%(id)s"
 
 class FlickrPlugin(CMSPluginBase):
     model = Flickr
@@ -29,17 +29,26 @@ class FlickrPlugin(CMSPluginBase):
             if instance.user_name:
                 rsp = f.people_findByUsername(username=instance.user_name)
                 user_id = [x for x in rsp][0].attrib['id']
-                kwargs.update({'user_id': user_id})                   
+                kwargs.update({'user_id': user_id})   
+                url = FLICKR_URL                
             if instance.tags:
                 kwargs.update({'tags': instance.tags,
                                'tag_mode': instance.tags_match})
+            if instance.group_id:
+                kwargs.update({'group_id': instance.group_id})
+                if not instance.user_name:
+                    url = FLICKR_GROUP_URL
             photos = f.photos_search(**kwargs)[0]
             for p in photos:
                 p.attrib['size'] = instance.size
+                if not instance.user_name:
+                    id = instance.group_id
+                else:
+                    id = p.attrib['id']
                 items.append({'title': p.attrib['title'],
                               'url': FLICKR_IMAGE_URL % p.attrib,
-                              'link': FLICKR_URL % {'user_id': user_id,
-                                                   'id': p.attrib['id']}})
+                              'link': url % {'user_id': 'user_id',
+                                                   'id': id}})
         except flickrapi.FlickrError, e:
             error = "FlickrAPI Error: " + str(e)
             print error
